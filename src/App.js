8d20cd758d6dbec3299f8607cca31748188668ab
Hasan Edu <hasantktl.edu@gmail.com>
2024-09-08 19:31:29 +0300

    Eleman dışında alan tıklanma durumunu tespit eden kod yazıldı.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useRef } from 'react';
 import './App.css';
+import { useOnClickOutside } from './hooks/useOnClickOutside';
 
 function App() {
+  const ref1 = useRef()
+  function disarisiTiklandi() {
+    alert("Elemanın dışında bir alan tıklandı!")
+  }
+  useOnClickOutside(ref1, disarisiTiklandi)
+
+  const ref2 = useRef()
+  function disarisiTiklandi2() {
+    alert("Elemanın dışında bir alan tıklandı! (Kutu 2)")
+  }
+  useOnClickOutside(ref2, disarisiTiklandi2)
+
   return (
     <>
-      App 1
+      <div ref={ref1} id='kutu1'>
+        Kutu 1
+      </div>
+
+      <div ref={ref2} id='kutu2'>
+        Kutu 2
+      </div>
     </>
   );
 }
