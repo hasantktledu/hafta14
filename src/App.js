64507f3bf34ba1f644d96d11eb900271f3c9be92
Hasan Edu <hasantktl.edu@gmail.com>
2024-09-08 19:42:56 +0300

    Pencere genişliğini takip eden hook örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import useWindowSize from './hooks/useWindowSize';
 
 function App() {
+  const pencere = useWindowSize()
+
+
   return (
     <>
-      App 1
+      <div>
+        Genişlik: {pencere.width} <br /> 
+        Yükseklik: {pencere.height} 
+      </div>
+
+      <div className={pencere.width<=600 ? 'gizle' : ''}>
+        REKLAM REKLAM REKLAM REKLAM REKLAM REKLAM REKLAM REKLAM 
+        REKLAM REKLAM REKLAM REKLAM REKLAM REKLAM REKLAM REKLAM 
+      </div>
+
+      {
+        pencere.width > 600 &&
+          <div>
+            REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2
+            REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2 REKLAM2
+          </div>
+      }     
     </>
   );
 }
