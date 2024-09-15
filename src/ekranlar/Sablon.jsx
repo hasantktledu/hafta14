52b30463d872dc27075ac8e2e67b3345120c721b
Hasan Tokatlı Edu <hasantktl.edu@gmail.com>
2024-09-15 16:22:43 +0300

    Temel router komponentleri incelendi.

--- src/ekranlar/Sablon.jsx
+++ src/ekranlar/Sablon.jsx
@@ -1,13 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Sablon() {
+  return (
+    <>
+      <header className="container">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary px-3">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/">
+                Ana Ekran
+              </Link>
+              <Link className="nav-link" to="/blog">
+                Blog
+              </Link>
+              <Link className="nav-link" to="/iletisim">
+                İletişim
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
 
-    
-    return (
-        <>
-            <header>Header alanı</header>
-            <Outlet />
-            <footer>Footer alanı</footer>
-        </>
-    )
+      <section className="container py-3">
+        <Outlet />
+      </section>
+
+      <footer className="container">Footer alanı</footer>
+    </>
+  );
 }
